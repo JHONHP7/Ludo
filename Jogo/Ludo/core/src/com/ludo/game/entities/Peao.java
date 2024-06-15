59a3a6b549b069400e25997589d8134005cd4205
Jhonatan linux <jhonatansilva@id.uff.br>
2024-06-15 19:44:46 -0300

    Enums criados

---
+++ Jogo/Ludo/core/src/com/ludo/game/entities/Peao.java
@@ -0,0 +1,6 @@
+package com.ludo.game.entities;
+
+public class Peao {
+    int idxCasaPeao = 0;
+    int id;
+}
---
+++ Jogo/Ludo/core/src/com/ludo/game/enums/StatusMovimentacao.java
@@ -0,0 +1,5 @@
+package com.ludo.game.enums;
+
+public enum StatusMovimentacao {
+	SUCESSO, FALHA
+}
