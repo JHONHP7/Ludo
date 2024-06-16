632d92d0165968aba087a43cc19bb3255d054f32
Roberto Martins <robertomartins@id.uff.br>
2024-06-15 21:54:49 -0300

    Atualiza classe tabulerio

--- Jogo/Ludo/core/src/com/ludo/game/entities/Tabuleiro.java
+++ Jogo/Ludo/core/src/com/ludo/game/entities/Tabuleiro.java
@@ -1,16 +1,25 @@
 package com.ludo.game.entities;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import com.ludo.game.abstracts.CasaAbstrata;
+import com.ludo.game.abstracts.Jogador;
 
 public class Tabuleiro {
     private final HashMap<Integer, CasaAbstrata> casas = new HashMap<>();
+    private final ArrayList<Jogador> jogadores = new ArrayList<>();
+
+
 
     // Adiciona uma casa ao tabuleiro
     public void addCasa(int posicao, CasaAbstrata casa) {
         if (casas.size() < 80) {
             casas.put(posicao, casa);
         }
+    }
+
+    public void addJogador(Jogador jogador) {
+        jogadores.add(jogador);
     }
 
     // Outros métodos do tabuleiro conforme necessário
