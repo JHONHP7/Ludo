4789550cdc33112ad2bddc21fc9d5db48331838a
Jhonatan linux <jhonatansilva@id.uff.br>
2024-06-15 21:22:01 -0300

    Ajuste resolução

--- Jogo/Ludo/core/src/com/ludo/game/Ludo.java
+++ Jogo/Ludo/core/src/com/ludo/game/Ludo.java
@@ -12,7 +12,7 @@ public class Ludo extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("tabuleiro.png");
 	}
 
 	@Override
--- Jogo/Ludo/desktop/src/com/ludo/game/DesktopLauncher.java
+++ Jogo/Ludo/desktop/src/com/ludo/game/DesktopLauncher.java
@@ -10,6 +10,8 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Ludo");
+		config.setWindowedMode(1920,1080);
 		new Lwjgl3Application(new Ludo(), config);
+
 	}
 }
